6a253ca522209cef0698a3cf2288b904894585f6
kfhwang <kfhwang@nutc.edu.tw>
2025-10-07 15:32:38 +0800

    20251007

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,4 @@
+//recursion O(2^n)
 function fib(n){
     //prevent n<0
     if(n==0||n==1)
@@ -5,6 +6,16 @@ function fib(n){
     else
        return fib(n-1)+fib(n-2);
 }
+// iteration version O(n)
+function fib(n){
+    //prevent n<0
+    var a=0,b=1;
+    for(var i=1;i<=n;i++){
+        a = a+b;
+        b = a-b;
+    }
+    return a;
+}
 //test
-var n=3
+var n=50
 console.log("fibonacci "+n+"="+fib(n));
